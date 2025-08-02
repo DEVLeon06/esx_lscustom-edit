aff9455034adac03aafb1c09d13b42e3d9e166d3
Leon06 <164539118+RLCLeon1@users.noreply.github.com>
2025-08-02 20:02:41 +0200

    Add files via upload

---
+++ esx_lscustom/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version 'adamant'
+
+game 'gta5'
+autor 'ESX, Leon06(Edit)'
+
+description 'Allows Players to use LS Customs to customise their cars'
+lua54 'yes'
+version '1.0'
+
+shared_script '@es_extended/imports.lua'
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'@es_extended/locale.lua',
+	'locales/*.lua',
+	'config.lua',
+	'server/main.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'locales/*.lua',
+	'config.lua',
+	'client/main.lua'
+}
